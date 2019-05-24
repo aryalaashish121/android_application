be2bc838d017613e1d34efed1b78e162644381ba
aryalaashish121 <aryalaashish121@gmail.com>
2019-05-25 01:02:08 +0545

    Drashboard design edited

--- app/src/main/java/com/example/online_store/MainActivity.java
+++ app/src/main/java/com/example/online_store/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private EditText search_tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
+        search_tab = findViewById(R.id.search_tab);
+
+
 
         ViewPagerFragmentAdapter adapter = new ViewPagerFragmentAdapter(getSupportFragmentManager());
         adapter.addFragment(new Fragment_Drashboard(),"Home");
